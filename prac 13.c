28a31526b6b6d085ad20e8f0b04a1cd2ac378b78
Harsh kumar <78996805+Harsh2227kumar@users.noreply.github.com>
2026-04-16 00:32:38 +0530

    Update prac 13.c

--- prac 13.c
+++ prac 13.c
@@ -2,56 +2,63 @@
 
 int main() {
     int n, m, i, j, k;
-    int alloc[20][20], max[20][20], need[20][20];
-    int avail[20], finish[20], safe[20];
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
+
     printf("Enter number of resources: ");
     scanf("%d", &m);
 
+    int alloc[100], max[100], need[100];
+    int avail[20], finish[20], safe[20];
+
     printf("Enter allocation matrix:\n");
-    for(i = 0; i < n; i++)
-        for(j = 0; j < m; j++)
-            scanf("%d", &alloc[i][j]);
+    for(i = 0; i < n * m; i++)
+        scanf("%d", &alloc[i]);
 
     printf("Enter max matrix:\n");
-    for(i = 0; i < n; i++)
-        for(j = 0; j < m; j++)
-            scanf("%d", &max[i][j]);
+    for(i = 0; i < n * m; i++)
+        scanf("%d", &max[i]);
 
     printf("Enter available resources:\n");
     for(i = 0; i < m; i++)
         scanf("%d", &avail[i]);
 
-    for(i = 0; i < n; i++)
-        for(j = 0; j < m; j++)
-            need[i][j] = max[i][j] - alloc[i][j];
+    // Calculate need
+    for(i = 0; i < n * m; i++)
+        need[i] = max[i] - alloc[i];
 
     for(i = 0; i < n; i++)
         finish[i] = 0;
 
     int count = 0;
+
     while(count < n) {
         int found = 0;
+
         for(i = 0; i < n; i++) {
             if(finish[i] == 0) {
                 int ok = 1;
-                for(j = 0; j < m; j++)
-                    if(need[i][j] > avail[j]) ok = 0;
+
+                for(j = 0; j < m; j++) {
+                    if(need[i * m + j] > avail[j]) {
+                        ok = 0;
+                        break;
+                    }
+                }
 
                 if(ok) {
                     for(k = 0; k < m; k++)
-                        avail[k] += alloc[i][k];
+                        avail[k] += alloc[i * m + k];
 
-                    safe[count] = i;
+                    safe[count++] = i;
                     finish[i] = 1;
-                    count++;
                     found = 1;
                 }
             }
         }
-        if(found == 0) {
+
+        if(!found) {
             printf("System is not in safe state\n");
             return 0;
         }
@@ -60,7 +67,6 @@ int main() {
     printf("Safe sequence: ");
     for(i = 0; i < n; i++)
         printf("P%d ", safe[i]);
-    printf("\n");
 
     return 0;
 }
